530c7295351dc2c50f9eb1b0fa733583a682df8f
Preston Garrison <preston@flightone.com>
2025-01-29 13:28:31 -0500

    merge changes

--- installer/create_package.sh
+++ installer/create_package.sh
@@ -1,11 +1,15 @@
-#!/bin/sh
+k#!/bin/sh
 DARKFLOWS_DIR="/usr/local/darkflows"
 PACKAGE_DIR="/usr/local/installer_packages"
+VERSION=$(head -n1 $DARKFLOWS_DIR/version.txt | tr -d '\r')  # Get cleaned version number
+
 rm $PACKAGE_DIR/*
 echo cp $DARKFLOWS_DIR/installer/install_darkflows.sh $PACKAGE_DIR/
 cp $DARKFLOWS_DIR/installer/install_darkflows.sh $PACKAGE_DIR/
 $DARKFLOWS_DIR/installer/create_backup.sh
 cd $PACKAGE_DIR
-tar zcvf /tmp/darkflows.tgz *
-echo "package is in /tmp/darkflows.tgz"
+tar zcvf /tmp/darkflows-${VERSION}.tgz *
+echo "package is in /tmp/darkflows-${VERSION}.tgz"
+scp -P 8020 /tmp/darkflows-${VERSION}.tgz root@darkflows.com:/var/www/darkflows.com/downloads/
+
 
